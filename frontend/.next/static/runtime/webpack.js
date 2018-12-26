e552208268e4b53bf9b40525cb3a9ac6a77b8c6c
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2018-12-25 23:58:03 -0800

    Add style to Nav bar

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1b8049b73effb1170d9a";
+/******/ 	var hotCurrentHash = "a2aa6bc3807f80df0e03";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
