868fcb8847d25160a1ab7b8f659cd963f217fb1b
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2018-12-28 13:42:26 -0800

    Set up Prisma Yoga Flow
    -Form sample mutation and query for GraphQL

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,24 @@
-const Mutations = {};
+const db = require('../db');
+
+const Mutations = {
+  //   createDog(parent, args, ctx, info) {
+  //     global.dogs = global.dogs || [];
+  //     const newDog = { name: args.name };
+  //     global.dogs.push(newDog);
+  //     return newDog;
+  //   }
+  async createItem(parent, args, ctx, info) {
+    console.log(ctx.db);
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
+};
 
 module.exports = Mutations;
