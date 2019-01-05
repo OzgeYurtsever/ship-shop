1ccb85c1dbca5babe564064aa82e83df67ed2c1f
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2019-01-04 17:18:18 -0800

    Complete reset password front end

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b6988da9cf099ba5179c";
+/******/ 	var hotCurrentHash = "7b75e8a3421d0bcd5290";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
